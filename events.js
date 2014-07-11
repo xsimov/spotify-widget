905f23b6dae1f87062cc56b43bd62034abfcc9d2
xsimov <xsimov@gmail.com>
2014-07-11 19:13:26 +0200

    Everything wroking, style issues and improvements still to be coded

--- events.js
+++ events.js
@@ -1,36 +1,34 @@
 (function(){
+"use strict";
 	
 document.getElementsByClassName('btn-play')[0].addEventListener('click', changePlayButton);
-var progress = document.getElementsByTagName('progress')[0];
+window.progress = document.getElementsByTagName('progress')[0];
 
-	function changePlayButton(event){
-		$event = $(event.target);
-		if (event.target.classList.contains('playing')){
-			$event.addClass('disabled');
-			$event.removeClass('playing');
-			audio.pause();
-		}
-		else {    //other way round
-			$event.addClass('playing');
-			$event.removeClass('disabled)');
-			audio.play();
-		};
+function changePlayButton(event){
+	if (event.target.className === 'btn-play disabled') {
+	        event.target.className = 'btn-play playing';
+	        audio.play();
+	    }
+	    else {
+	        event.target.className = 'btn-play disabled';
+	        audio.pause();
+	    };
 	};
 
-	audio.addEventListener("timeupdate", function (){
-		progress.value = audio.currentTime / audio.duration * 100;
-		console.log(progress.value);
- 	  	});
+audio.addEventListener("timeupdate", function (){
+	progress.value = audio.currentTime / audio.duration * 100;
+	console.log(progress.value);
+	  	});
 
-	// document.getElementsByTagName('form')[0].addEventListener('onsubmit', formatQ);
+document.getElementsByTagName('form')[0].addEventListener('submit', formatQ);
 
-	function formatQ(event){
-		event.preventDefault();
-		var name = document.forms['form']['name'].value;
-		var q = name.replace(" ", "%20");
-		q += "&type=track";
-		alert(q);
-		// event.returnValue = false;
+function formatQ(event){
+	event.preventDefault();
+	var name = document.forms['form']['name'].value;
+	var q = name.replace(" ", "%20");
+	q = "https://api.spotify.com/v1/search?q=" + q + "&type=track&limit=10";
+	console.log(q);
+	getTrack(q,1);
 	};
 
 })(window);
--- script.js
+++ script.js
@@ -10,7 +10,7 @@
 	// var artist = document.getElementsByClassName('artist')[0];
 	// var song = document.getElementsByClassName('song')[0];
 
-function getTrack(queryURL,listFlag){
+window.getTrack = function(queryURL,listFlag){
 	var xhr = new XMLHttpRequest();
     xhr.open('GET', queryURL);
 
@@ -47,20 +47,30 @@ function displayTable(response){
 	var cell2;
 	var cell3;
 	var img;
+	var link;
 
 	for (var i = 0; i < response.tracks.items.length; i++) {
 		row = table.insertRow(i);
 		cell1 = row.insertCell(0);
 		cell2 = row.insertCell(1);
 		cell3 = row.insertCell(2);
+		link = document.createElement('a');
+		link.href = response.tracks.items[i].href;
+		link.className = "hidden";
 		img = document.createElement('img');
 		img.src = response.tracks.items[i].album.images[2].url; //urlIMG
 		cell1.appendChild(img);
+		cell1.appendChild(link);
 		cell2.innerHTML = response.tracks.items[i].name;
 		cell3.innerHTML = response.tracks.items[i].artists[0].name;  //ALL THE ARTISTS!
 		};
 
 	document.getElementsByClassName('table')[0].appendChild(table);
+	document.getElementsByClassName('table')[0].className = "table";
+	document.getElementsByTagName('tbody')[0].addEventListener('click', function(){
+	var url = event.target.parentNode.firstChild.lastChild.href;
+	getTrack(url,0);
+	});
 };
 
 
